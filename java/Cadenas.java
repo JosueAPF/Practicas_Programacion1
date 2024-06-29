a72929a470f27acbf2b72b0d5f99c5fbac8cadfb
josue <josueporras124@gmail.com>
2024-06-29 16:51:48 -0600

    Ejercicios con cadenas en java

--- java/Cadenas.java
+++ java/Cadenas.java
@@ -1,6 +1,53 @@
+//operaciones basicas 
+import java.util.Scanner;
+
+
+class CadenasDeclaracion{
+   protected String Cadena1;
+   protected String Cadena2;
+   protected int opc;
+   protected Scanner in;
+   
+   public CadenasDeclaracion(){
+      Cadena1 = "hola";
+      Cadena2 = "MUNDO";
+      in = new Scanner(System.in);
+   }
+}
+
+class OperacionsCadenas extends CadenasDeclaracion{
+   
+   public void Concatenacion(){
+      System.out.println("Operacion : -Concatenacion-");
+      System.out.println(Cadena1+" "+Cadena2);
+   }
+   
+   public void Convercion(){
+      System.out.println("Ingrese un opcion");
+      System.out.println("1. Convertir a mayuscula");
+      System.out.println("2. convertir a minuscula");
+      opc = in.nextInt();
+      switch(opc){
+         case 1:
+            System.out.println("-Mayuscula-");
+            System.out.println(Cadena1.toUpperCase());
+            break;
+         case 2:
+            System.out.println("-Minuscula-");
+            System.out.println(Cadena2.toLowerCase());
+            break;
+         default:
+            break;
+      }
+   }
+
+}
+
 public class Cadenas{
 	public static void main(String[] args){
-		System.out.println("Hola mundo");
+      OperacionsCadenas cadena = new OperacionsCadenas();
+      cadena.Concatenacion();
+      cadena.Convercion();
 	} 
 	
 }
