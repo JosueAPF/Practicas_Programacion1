d42213d39f1432305fe32485bb0c22fcb8664843
josue <josueporras124@gmail.com>
2024-07-11 09:22:36 -0600

    update de class_2

--- c++/Class_2.cpp
+++ c++/Class_2.cpp
@@ -1,10 +1,76 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 
-int main(){
-	cout<<"hola mundo"<<endl;
+//super class
+class Persona{
 
+//encapsulamiento
+
+	//proceso de abstraccion
+	protected:
+		string Nombre;
+		string Ape;
+		int edad;
+		float peso,altura;
+		int ano_nacim;
+	public:
+		Persona(string Nombre,string Ape,int edad,float peso,float altura,int ano_nacim){
+			this->Nombre = Nombre;
+			this->Ape = Ape;
+			this->edad  = edad;
+			this->peso = peso;
+			this->altura = altura;
+			this->ano_nacim = ano_nacim;
+		}
+		
+		
+		void Datos(){
+			cout<<"Datos cmpletos de :"<<this->Nombre<<endl;
+			cout<<"Apellido :"<<this->Ape<<endl;
+			cout<<"Edad :"<<this->Ape<<endl;
+			cout<<"Peso :"<<this->peso<<endl;
+			cout<<"Altura :"<<this->altura<<endl;
+			cout<<"Año de nacimiento :"<<this->ano_nacim<<endl;
+			cout<<endl;
+		}
+		
+		string myNombre(){
+			return this->Nombre;
+		}
+};
+
+
+//sub class
+class jose:public Persona{
+	private:
+		string SegApellido;//segundo apellido
+	public:
+		jose(string SegApellido,string Nombre,string Ape,int edad,float peso,float altura,int ano_nacim):Persona(Nombre,Ape,edad,peso,altura,ano_nacim){
+			this->SegApellido = SegApellido;
+		}
+		
+		void fulldata(){
+			cout<<"Datos cmpletos de :"<<this->Nombre<<endl;
+			cout<<"Apellido :"<<this->Ape<<endl;
+			cout<<"Second Apellido :"<<this->SegApellido<<endl;
+			cout<<"Edad :"<<this->Ape<<endl;
+			cout<<"Peso :"<<this->peso<<endl;
+			cout<<"Altura :"<<this->altura<<endl;
+			cout<<"Año de nacimiento :"<<this->ano_nacim<<endl;
+			cout<<endl;
+		}
+};
+
+
+//otra subclass
+
+
+int main(){
+	jose obj1("perez","jose","Hernandez",18,2.5,1.60,1800);
+	
+	obj1.fulldata();
 	return 0;
 }
